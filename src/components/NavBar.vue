c04a7628821f2c0a4ae11d519da119cb493aefbd
Svetlana Bisacki <svetlanabisacki@gmail.com>
2018-06-12 21:16:00 +0200

    bootstrap -nav-bar componenta -ubacen bootstrap u projekat

---
+++ src/components/NavBar.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="hello">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">All Galleries</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Login<span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Register<span class="sr-only">(current)</span></a>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-secondary my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.navbar {
+background-image:
+
+  url('https://st3.depositphotos.com/12674628/15278/i/450/depositphotos_152785580-stock-photo-black-slate-background.jpg');
+}
+.navbar-brand {
+  color: whitesmoke !important;
+}
+.nav-link {
+  color: whitesmoke !important;
+}
+.btn {
+  color: whitesmoke !important;
+  background: rgb(66, 65, 65);
+}
+</style>
